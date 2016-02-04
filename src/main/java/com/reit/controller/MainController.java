56e0a48fb2ef8b1d2b13ac18366558da4a5ff243
kbouzidi <kr.bouzidi@gmail.com>
2016-02-04 23:54:55 +0100

    Add routes

--- src/main/java/com/reit/controller/MainController.java
+++ src/main/java/com/reit/controller/MainController.java
@@ -2,12 +2,25 @@ package com.reit.controller;
 
 import com.reit.service.TodoService;
 
-/***
- * Created by khalil on 04/02/16.
- */
+import static spark.Spark.get;
+import static spark.Spark.staticFileLocation;
+
+
 public class MainController {
 
     public MainController(final TodoService todoService) {
 
+        // Set static file location {/resources/public}
+        staticFileLocation("/public");
+
+
+        get("/", (request, response) -> {
+            return "Done";
+        });
+
+        // Health check
+        get("/ping", (request, response) -> {
+            return "pong";
+        });
     }
 }
