7b020f6f0e0474b364fc19532709b24d81465ec2
kbouzidi <kr.bouzidi@gmail.com>
2016-02-05 20:59:08 +0100

    Add public website

---
+++ src/main/resources/public/lib/main.js
@@ -0,0 +1,37 @@
+angular.module('todowebapp', [
+    'ngRoute'
+])
+    .config(function ($routeProvider) {
+        $routeProvider.when('/', {
+            templateUrl: 'views/todos.html',
+            controller: 'AllCtrl'
+        }).when('/add', {
+            templateUrl: 'views/add.html',
+            controller: 'AddCtrl'
+        }).otherwise({
+            redirectTo: '/'
+        })
+    })
+
+    .controller('AllCtrl', function ($scope, $http) {
+        $http.get('/todos').success(function (data) {
+            $scope.todos = data;
+        }).error(function (err, status) {
+            console.log('Error ' + err)
+        });
+
+
+        $scope.updateTodo = function (todo) {
+            $http.put('/update' + todo.id, todo).success(function (data) {
+                console.log('status changed');
+            }).error(function (data, status) {
+                console.log('Error ' + data)
+            })
+        }
+
+    })
+
+    .controller('AddCtrl', function ($scope, $http) {
+
+    })
+;
