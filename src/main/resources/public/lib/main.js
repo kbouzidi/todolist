1fb197a4ea2808744af2e0cb08e92bcb4127c80f
kbouzidi <kr.bouzidi@gmail.com>
2016-02-07 01:09:22 +0100

    Update FE

--- src/main/resources/public/lib/main.js
+++ src/main/resources/public/lib/main.js
@@ -1,19 +1,61 @@
 angular.module('todowebapp', [
-    'ngRoute'
+    'ui.router'
 ])
-    .config(function ($routeProvider) {
-        $routeProvider.when('/', {
-            templateUrl: 'views/todos.html',
-            controller: 'AllCtrl'
-        }).when('/add', {
-            templateUrl: 'views/add.html',
-            controller: 'AddCtrl'
-        }).otherwise({
-            redirectTo: '/'
-        })
+    .config(function ($urlRouterProvider, $stateProvider) {
+        /*$routeProvider.when('/', {
+         templateUrl: 'views/todos.html',
+         controller: 'MainCtrl'
+         }).when('/add', {
+         templateUrl: 'views/add.html',
+         controller: 'AddCtrl'
+         }).when('/login', {
+         templateUrl: 'views/login.html',
+         controller: 'LoginCtrl'
+         }).when('/todos', {
+         templateUrl: 'views/todos.html',
+         controller: 'TodosCtrl'
+         }).otherwise({
+         redirectTo: '/'
+         })*/
+
+
+        // For any unmatched url, send to /index
+        $urlRouterProvider.otherwise("/todos");
+
+        $stateProvider
+            .state('login', {
+                url: "/login",
+                templateUrl: "views/login.html",
+                controller: "LoginCtrl"
+            })
+            .state('home', {
+                url: "/todos",
+                templateUrl: "views/todos.html",
+                controller: "TodosCtrl"
+            })
+            .state('add', {
+                url: "/add",
+                templateUrl: "views/add.html",
+                controller: "AddCtrl"
+            });
+
+
+    })
+    .controller('MainCtrl', function ($scope, $http, $location, $rootScope) {
+        if (!$rootScope.user) {
+            $location.path("/login");
+            if (!$scope.$$phase) {
+                $scope.$apply();
+            }
+        } else {
+            $location.path('/todos');
+
+        }
     })
 
-    .controller('AllCtrl', function ($scope, $http) {
+    .controller('TodosCtrl', function ($scope, $http, $location, $rootScope) {
+
+        $scope.todos = {};
         $http.get('/todos').success(function (data) {
             $scope.todos = data;
         }).error(function (err, status) {
@@ -30,8 +72,16 @@ angular.module('todowebapp', [
         }
 
     })
+    .controller('LoginCtrl', function ($scope, $http, $location, $rootScope) {
 
+        $scope.connect = function (logged) {
+            $rootScope.user = logged;
+            $location.path('/todos');
+        }
+
+    })
     .controller('AddCtrl', function ($scope, $http, $location) {
+
         $scope.todo = {
             state: "STARTED"
         };
